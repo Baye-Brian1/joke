a8028ace265fbe2d22740e0c97dd980d2a220f47
brian <bayebrian89@gmail.com>
2025-09-11 15:41:01 +0100

    new update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
  const btn= document.querySelector('.btn');
  const content = document.querySelector('.content')
+ const container= document.querySelector('.container')
 
 const URL = 'https://api.chucknorris.io/jokes/random?category=dev';
 
@@ -21,26 +22,5 @@ btn.addEventListener('click', async()=>{
 function displayData(data){
   const {value: joke}= data;
   content.textContent= joke;
+  container.appendChild(content)
 }
-
-// HERE I USED AJAX SYNTAX(xhr) TO understand how to get apis and i equally used promises
-// function getData(url) {
-//   return new Promise((resolve, reject)=>{
-//     const xhr = new XMLHttpRequest();
-//   xhr.open('GET', url);
-//   xhr.send();
-//   xhr.onreadystatechange= function(){
-//     if (xhr.readyState !==4) return;
-//     if (xhr.status ==200) {
-//     resolve(xhr.responseText)
-//     }else{
-//       reject({
-//         status:xhr.status,
-//         text:xhr.text,
-//       });
-      
-//     }
-//   };  
-//   });
-  
-// }
